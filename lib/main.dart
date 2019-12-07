72984db7ef157703c93377377208bda3ee6399b6
Raushan Raja <vraushanraja@gmail.com>
2019-12-08 00:50:26 +0530

    Andriod Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'components/listTile/ListTile_screen.dart';
+import 'package:flutter_ui_components/models/UIList.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,18 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'UI Comps'),
     );
   }
 }
@@ -44,6 +37,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Uilist> uilist = Uilist.uilist;
+
   int _counter = 0;
 
   void _incrementCounter() {
@@ -71,41 +66,23 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: uilist.length,
+        itemBuilder: (context,index){
+          return ListTile(
+            leading: Text('${uilist[index].uiNumber}'),
+            title: Text('${uilist[index].uiName}'),
+            onTap:(){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ListTileScreen(title:'${uilist[index].uiName}')));
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }
---
+++ lib/models/UIList.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+
+class Uilist {
+
+  final int uiNumber;
+  final String uiName; 
+  Uilist({
+    this.uiNumber,
+    this.uiName,
+  });
+  
+
+  Uilist copyWith({
+    int uiNumber,
+    String uiName,
+  }) {
+    return Uilist(
+      uiNumber: uiNumber ?? this.uiNumber,
+      uiName: uiName ?? this.uiName,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uiNumber': uiNumber,
+      'uiName': uiName,
+    };
+  }
+
+  static Uilist fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+  
+    return Uilist(
+      uiNumber: map['uiNumber'],
+      uiName: map['uiName'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  static Uilist fromJson(String source) => fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Uilist uiNumber: $uiNumber, uiName: $uiName';
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+  
+    return o is Uilist &&
+      o.uiNumber == uiNumber &&
+      o.uiName == uiName;
+  }
+
+  @override
+  int get hashCode => uiNumber.hashCode ^ uiName.hashCode;
+
+  static List<Uilist> uilist =[
+    Uilist(uiNumber: 1, uiName: "ListTile"),];
+}
