701037dacbae92ef5314d713515d2129ea957fe2
raushanraja <vraushanraja@gmail.com>
2020-01-16 00:41:33 +0530

    Added more inputs

--- lib/screens/admission_screen.dart
+++ lib/screens/admission_screen.dart
@@ -14,18 +14,18 @@ class _AdmissionScreenState extends State<AdmissionScreen> {
   String fatherName;
   String motherName;
   String gender;
-  String village;
-  String ward;
-  String category;
-  String dob;
-  String minority;
-  String divyang;
-  String aadharNo;
-  String mobileNo;
-  String ifscCode;
-  String bankAccountNo;
-  String bankAccountName;
-  String annualIncome;
+  String village="V";
+  String ward="W";
+  String category="C";
+  String dob="12-10-1997";
+  String minority="0";
+  String divyang="1";
+  String aadharNo="1212";
+  String mobileNo="4545";
+  String ifscCode="8848";
+  String bankAccountNo="sdfsd8";
+  String bankAccountName="s8888d/d/d/";
+  String annualIncome="99998898";
 
 
   @override
@@ -35,15 +35,55 @@ class _AdmissionScreenState extends State<AdmissionScreen> {
         centerTitle: true,
         title: Text("Admission"),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        onPressed: () async{
+          print("Adding Data");
+        },
+      ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             TextField(
+              decoration: InputDecoration(
+                hintText: "admissionNo"
+              ),
               onChanged: (value){
-                id=int.tryParse(value);
+                admissionNo=value;
               },
-            )
+            ),
+             TextField(
+              decoration: InputDecoration(
+                hintText: "admissionDate"
+              ),
+              onChanged: (value){
+                admissionDate = value;
+              },
+            ),
+             TextField(
+              decoration: InputDecoration(
+                hintText: "studentName"
+              ),
+              onChanged: (value){
+                studentName = value;
+              },
+            ),
+             TextField(
+              decoration: InputDecoration(
+                hintText: "fatherName"
+              ),
+              onChanged: (value){
+                fatherName = value;
+              },
+            ), TextField(
+              decoration: InputDecoration(
+                hintText: "motherName"
+              ),
+              onChanged: (value){
+                motherName = value;
+              },
+            ),
           ],
         ),
       ),
