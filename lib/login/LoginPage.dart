529ede5df246e544052ccfb5003b205939cce6b3
raushanraja <vraushanraja@gmail.com>
2020-03-01 23:58:02 +0530

    Hide Keyboard

--- lib/login/LoginPage.dart
+++ lib/login/LoginPage.dart
@@ -4,6 +4,8 @@ import 'package:login_flow/authentication/Firebase_Auth.dart';
 import 'package:login_flow/main.dart';
 import 'package:provider/provider.dart';
 
+import 'package:flutter/services.dart';
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -13,6 +15,7 @@ class _LoginPageState extends State<LoginPage> {
   final _formLogin = GlobalKey<FormState>();
   final _formSignup = GlobalKey<FormState>();
 
+  bool isLoading = false;
   String _name;
   String _emailID;
   String _password;
@@ -20,6 +23,7 @@ class _LoginPageState extends State<LoginPage> {
   var _emailIDController = TextEditingController();
   var _passwordController = TextEditingController();
   var _nameController = TextEditingController();
+
   final TextStyle textstyle =
       TextStyle(color: Colors.white, fontWeight: FontWeight.bold);
 
@@ -39,6 +43,12 @@ class _LoginPageState extends State<LoginPage> {
     super.dispose();
   }
 
+  void updateLoader() {
+    setState(() {
+      isLoading = !isLoading;
+    });
+  }
+
   void _changeEmailvalue() {
     _emailID = _emailIDController.text;
   }
@@ -46,7 +56,6 @@ class _LoginPageState extends State<LoginPage> {
   void _changePasswordValue() {
     _password = _passwordController.text;
   }
-
 
   void _changeNameValue() {
     _name = _nameController.text;
@@ -56,131 +65,164 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-                maxHeight: MediaQuery.of(context).size.height -
-                    (MediaQuery.of(context).size.height) * 5 / 100),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                FlutterLogo(
-                  size: 80,
-                ),
-                DefaultTabController(
-                    length: 2,
-                    initialIndex: 0,
-                    child: Column(
-                      children: <Widget>[
-                        TabBar(
-                          tabs: [
-                            BuildTab(tabLabel: "SIGN IN"),
-                            BuildTab(tabLabel: "SIGN UP"),
-                          ],
-                        ),
-                        ConstrainedBox(
-                          constraints: BoxConstraints(
-                            maxHeight: 300,
-                          ),
-                          child: TabBarView(children: [
-                            FormBuild(
-                              formLogin: _formLogin,
-                              isSingup: false,
-                              textstyle: textstyle,
-                              singButton: SSignButton(
+        body: Stack(
+          children: <Widget>[
+            SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                    maxHeight: MediaQuery.of(context).size.height -
+                        (MediaQuery.of(context).size.height) * 5 / 100),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    FlutterLogo(
+                      size: 80,
+                    ),
+                    DefaultTabController(
+                        length: 2,
+                        initialIndex: 0,
+                        child: Column(
+                          children: <Widget>[
+                            TabBar(
+                              tabs: [
+                                BuildTab(tabLabel: "SIGN IN"),
+                                BuildTab(tabLabel: "SIGN UP"),
+                              ],
+                            ),
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                maxHeight: 300,
+                              ),
+                              child: TabBarView(children: [
+                                FormBuild(
+                                  formLogin: _formLogin,
+                                  isSingup: false,
                                   textstyle: textstyle,
-                                  buttonLabel: "Login",
-                                  onTap: () async {
-                                    var user = await Provider.of<Auth>(context,
-                                            listen: false)
-                                        .emailLogin(_emailID, _password);
-                                    if (user["error"]==null) {
-                                      print(user["user"].email);
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) => MyHomePage(
-                                              title: '${user["name"]}',
+                                  singButton: SSignButton(
+                                      textstyle: textstyle,
+                                      buttonLabel: "Login",
+                                      onTap: () async {
+                                        SystemChannels.textInput
+                                            .invokeMethod('TextInput.hide');
+                                        updateLoader();
+                                        var user = await Provider.of<Auth>(
+                                                context,
+                                                listen: false)
+                                            .emailLogin(_emailID, _password);
+                                        if (user["error"] == null) {
+                                          print(user["user"].email);
+                                          updateLoader();
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => MyHomePage(
+                                                title: '${user["name"]}',
+                                              ),
                                             ),
-                                          ),
-                                        );
-                                    }
-                                  },
-                                  buttonColor: Colors.blueAccent),
-                              textEditingController: [_emailIDController,_passwordController],
-                        
-                            ),
-                            FormBuild(
-                              formLogin: _formSignup,
-                              isSingup: true,
-                              textstyle: textstyle,
-                              singButton: SSignButton(
+                                          );
+                                        } else {
+                                          updateLoader();
+                                        }
+                                      },
+                                      buttonColor: Colors.blueAccent),
+                                  textEditingController: [
+                                    _emailIDController,
+                                    _passwordController
+                                  ],
+                                ),
+                                FormBuild(
+                                  formLogin: _formSignup,
+                                  isSingup: true,
                                   textstyle: textstyle,
-                                  buttonLabel: "Sign UP",
-                                  onTap: () async {
-                                    if (_password != null || _emailID != null || _name!=null) {
-                                      var user = await Provider.of<Auth>(
-                                              context,
-                                              listen: false)
-                                          .handleRegistration( _emailID, _password,_name);
-                                      if (user["error"] == null) {
-                                       print(user["user"].email);
-                                      }else{
-                                        print(user["error"]);
-                                      }
-                                    }
-                                  },
-                                  buttonColor: Colors.blueAccent),
-                              textEditingController: [_emailIDController,_passwordController,_nameController],
-                            
+                                  singButton: SSignButton(
+                                      textstyle: textstyle,
+                                      buttonLabel: "Sign UP",
+                                      onTap: () async {
+                                        if (_password != null ||
+                                            _emailID != null ||
+                                            _name != null) {
+                                          var user = await Provider.of<Auth>(
+                                                  context,
+                                                  listen: false)
+                                              .handleRegistration(
+                                                  _emailID, _password, _name);
+                                          if (user["error"] == null) {
+                                            print(user["user"].email);
+                                          } else {
+                                            print(user["error"]);
+                                          }
+                                        }
+                                      },
+                                      buttonColor: Colors.blueAccent),
+                                  textEditingController: [
+                                    _emailIDController,
+                                    _passwordController,
+                                    _nameController
+                                  ],
+                                ),
+                              ]),
                             ),
-                          ]),
-                        ),
-                      ],
-                    )),
-                SizedBox(
-                  height: 30,
-                  child: Text(
-                    'or',
-                    style: TextStyle(color: Colors.black38),
-                    textAlign: TextAlign.center,
-                  ),
+                          ],
+                        )),
+                    SizedBox(
+                      height: 30,
+                      child: Text(
+                        'or',
+                        style: TextStyle(color: Colors.black38),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    SSignButton(
+                      textstyle: textstyle,
+                      buttonLabel: 'Google',
+                      onTap: () async {
+                        FirebaseUser user =
+                            await Provider.of<Auth>(context, listen: false)
+                                .handleSignIn();
+                        if (user != null) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MyHomePage(
+                                title: '${user.displayName}',
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                      buttonColor: Colors.red,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, right: 20, top: 30),
+                      child: GestureDetector(
+                        onTap: () {
+                          print("Forgot Password");
+                        },
+                        child: Text("Forgot Password?",
+                            style:
+                                TextStyle(fontSize: 15, color: Colors.black45),
+                            textAlign: TextAlign.center),
+                      ),
+                    ),
+                  ],
                 ),
-                SSignButton(
-                  textstyle: textstyle,
-                  buttonLabel: 'Google',
-                  onTap: () async {
-                    FirebaseUser user =
-                        await Provider.of<Auth>(context, listen: false)
-                            .handleSignIn();
-                    if (user != null) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MyHomePage(
-                            title: '${user.displayName}',
-                          ),
-                        ),
-                      );
-                    }
-                  },
-                  buttonColor: Colors.red,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 20, right: 20, top: 30),
-                  child: GestureDetector(
-                    onTap: () {
-                      print("Forgot Password");
-                    },
-                    child: Text("Forgot Password?",
-                        style: TextStyle(fontSize: 15, color: Colors.black45),
-                        textAlign: TextAlign.center),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
+            isLoading
+                ? Stack(
+                    children: <Widget>[
+                      Opacity(
+                          opacity: 0.2,
+                          child: ModalBarrier(
+                              dismissible: false, color: Colors.grey)),
+                      Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    ],
+                  )
+                : Container(),
+          ],
         ),
       ),
     );
@@ -188,13 +230,12 @@ class _LoginPageState extends State<LoginPage> {
 }
 
 class FormBuild extends StatelessWidget {
-  FormBuild({
-    @required GlobalKey<FormState> this.formLogin,
-    @required this.textstyle,
-    @required this.singButton,
-    @required this.textEditingController,
-    @required this.isSingup
-  });
+  FormBuild(
+      {@required GlobalKey<FormState> this.formLogin,
+      @required this.textstyle,
+      @required this.singButton,
+      @required this.textEditingController,
+      @required this.isSingup});
   final GlobalKey<FormState> formLogin;
   final TextStyle textstyle;
   final Widget singButton;
@@ -209,13 +250,15 @@ class FormBuild extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-         Visibility(
-           visible: isSingup,
-           child: InputField(
-            label: "Name",
-            onchange: (){},
-            textEditingController: textEditingController[textEditingController.length-1],
-          ),),
+          Visibility(
+            visible: isSingup,
+            child: InputField(
+              label: "Name",
+              onchange: () {},
+              textEditingController:
+                  textEditingController[textEditingController.length - 1],
+            ),
+          ),
           InputField(
             label: "Email",
             onchange: () {},
