b6f1d8b2c7d788720845a037cebbc8d70a435dce
raushanraja <vraushanraja@gmail.com>
2021-04-07 22:46:59 +0530

    Updated SCV

---
+++ lib/size_helper.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static double _screenWidth;
+  static double _screenHeight;
+  static double _blockWidth = 0;
+  static double _blockHeight = 0;
+
+  static double textMultiplier;
+  static double imageSizeMultiplier;
+  static double heightMultiplier;
+  static double widthMultiplier;
+  static bool isPortrait = true;
+  static bool isMobilePortrait = false;
+
+  void init(BoxConstraints constraints, Orientation orientation) {
+    if (orientation == Orientation.portrait) {
+      _screenWidth = constraints.maxWidth;
+      _screenHeight = constraints.maxHeight;
+      isPortrait = true;
+      if (_screenWidth < 450) {
+        isMobilePortrait = true;
+      }
+    } else {
+      _screenWidth = constraints.maxHeight;
+      _screenHeight = constraints.maxWidth;
+      isPortrait = false;
+      isMobilePortrait = false;
+    }
+
+    _blockWidth = _screenWidth / 100;
+    _blockHeight = _screenHeight / 100;
+
+    textMultiplier = _blockHeight;
+    imageSizeMultiplier = _blockWidth;
+    heightMultiplier = _blockHeight;
+    widthMultiplier = _blockWidth;
+
+    debugPrint(_screenWidth.toString());
+  }
+}
+
+Size displaySize(BuildContext context) {
+  debugPrint('Size = ' + MediaQuery.of(context).size.toString());
+  return MediaQuery.of(context).size;
+}
+
+double displayHeight(BuildContext context) {
+  debugPrint('Height = ' + displaySize(context).height.toString());
+  return displaySize(context).height;
+}
+
+double displayWidth(BuildContext context) {
+  debugPrint('Width = ' + displaySize(context).width.toString());
+  return displaySize(context).width;
+}
+
+double displayHeightAppStatusBar(BuildContext context) {
+  double height = displayHeight(context) -
+      MediaQuery.of(context).padding.top -
+      kToolbarHeight;
+  debugPrint('Height with Appbar + Status = ' + height.toString());
+  return height;
+}
+
+double displayHeightStatusBarOnly(BuildContext context) {
+  double height = displayHeight(context) - MediaQuery.of(context).padding.top;
+  debugPrint('Height for Status bar only = ' + height.toString());
+  return height;
+}
+
+EdgeInsets spaceFromStatusbar(BuildContext context) {
+  return EdgeInsets.only(top: MediaQuery.of(context).padding.top);
+}
